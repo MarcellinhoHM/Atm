1c8b972f1d8f2196a23a2f50f884d0f4c267035c
Marcello Montella <126472446+MarcellinhoHM@users.noreply.github.com>
2024-11-06 15:46:29 -0300

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "global.c"
+
+void menu() {// vai abrir o menu
+    printf("1. Deposito\n");
+    printf("2. Saque\n");
+    printf("3. Checar saldo\n");
+    printf("4. Sair\n");
+}
